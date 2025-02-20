4169a14f82ab3f2a6d13e161bf950e8ec6bc248b
enora <enorasun1120@gmail.com>
2025-02-19 22:51:19 -0800

    Started home page and about page

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 const About = () =>{
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
-            <h1 className="text-4xl font-bold mb-4">About Me</h1>
+            <h1 className="text-4xl text-sky-400 font-bold mb-4">About Me</h1>
             <p className="text-lg text-gray-700 max-w-2xl text-center">
             Hi! My name is Enora, a Computer Science student at UBC. 
             I love building projects that promote **gender equality** in tech and exploring innovative ways to use **technology for social good**.
             </p>
 
             <div className="mt-6">
-                <h2 className="text-2xl font-semibold"> Fun Facts</h2>
+                <h2 className="text-2xl text-sky-400 font-semibold text-center"> Fun Facts</h2>
                 <ul className="mt-2 list-disc text-gray-600">
                     <li>🌟 I have a twelve year old ipod that still works (barely).</li>
                     <li>🎨 If I could talk to one philosopher, I would chose Michel Foucault.</li>
