c45fc0e404d7ca8fb90b341e72fd242ebc2982d5
enora <enorasun1120@gmail.com>
2025-02-19 22:27:17 -0800

    Created four pages

---
+++ my-app/src/app/about.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const About = () =>{
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
+            <h1 className="text-4xl font-bold mb-4">About Me</h1>
+            <p className="text-lg text-gray-700 max-w-2xl text-center">
+            Hi! My name is Enora, a Computer Science student at UBC. 
+            I love building projects that promote **gender equality** in tech and exploring innovative ways to use **technology for social good**.
+            </p>
+
+            <div className="mt-6">
+                <h2 className="text-2xl font-semibold"> Fun Facts</h2>
+                <ul className="mt-2 list-disc text-gray-600">
+                    <li>🌟 I have a twelve year old ipod that still works (barely).</li>
+                    <li>🎨 If I could talk to one philosopher, I would chose Michel Foucault.</li>
+                    <li>🎮 Merry is my favorite Animal Crossing character. </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default About;
