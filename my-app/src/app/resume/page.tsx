c3ec1082f511b6636d4e9956cef11bc4713baa06
enora <enorasun1120@gmail.com>
2025-02-20 19:43:15 -0800

    Added space holder for project and resume pages.

---
+++ my-app/src/app/resume/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Resume = () =>{
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
+            <h1 className="text-4xl text-sky-400 font-bold mb-4">Resume</h1>
+            <p className="text-lg text-gray-700 max-w-2xl text-center">
+            Space holder for my resume and contact info.
+            </p>
+            </div>
+            )
+}
+
+export default Resume;
