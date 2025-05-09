4e37ed4390ca3b5e136860c9c13a86014150e751
enora <enorasun1120@gmail.com>
2025-05-09 01:41:23 -0700

    Added gradient background to home page

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,15 +3,21 @@ import Link from "next/link";
 export default function Home() {
   return (
 
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col relative overflow-hidden">
+      {/* Glowing Rotating Blob Background */}
+      <div className="blob-outer-container">
+        <div className="blob-inner-container">
+          <div className="blob"></div>
+        </div>
+      </div>
       {/* Top Navigation Bar */}
-      <nav className="flex justify-between p-4">
+      <nav className="flex justify-between p-5">
       <div className="flex justify-between w-full max-w-6xl mx-auto">
           {["about", "project", "resume"].map((page) => (
             <Link
               key={page}
               href={`/${page}`}
-              className="relative group text-black"
+              className="relative group text-black text-2xl font-mono font-extralight"
             >
               <span className="transition-colors duration-300">{page.charAt(0).toUpperCase() + page.slice(1)}</span>
               <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
@@ -21,9 +27,9 @@ export default function Home() {
       </nav>
 
       {/* Centered Page Content */}
-      <div className="flex flex-col items-center justify-center flex-grow">
-        <h1 className="text-8xl font-bold">Enora Sun</h1>
-        <p className="text-2xl mt-4">Welcome to my portfolio!</p>
+      <div className="pointer-events-none absolute inset-0 flex flex-col items-center justify-center flex-grow z-10">
+        <h1 className="text-9xl font-bold font-fancy text-transparent bg-clip-text bg-gradient-to-br from-black/90 to-black/40 opacity-0 translate-y-4 animate-fade-in">Enora Sun</h1>
+        <p className="text-3xl mt-4 font-mono">Welcome to my portfolio!</p>
       </div>
     </div>
   );
