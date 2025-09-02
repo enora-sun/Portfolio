d422e58c082e93db3295140101bf892a2457c19a
enora <enorasun1120@gmail.com>
2025-09-01 21:13:43 -0700

    Fixed font on home page

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const boheme = localFont({
+  src: "/fonts/BohemeFloral.woff",
+  variable: "--font-boheme",
+  display: "swap",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${boheme.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
